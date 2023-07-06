a626634ff10cf0338316d46bdb8814d5de3a1dab
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-07-06 10:50:14 -0500

    Update Program.cs

--- 401Lab01/Program.cs
+++ 401Lab01/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class labB
+{
+    static void StartSequence()
+    {
+        
+    }
+
+    static void Main(string[] args)
+    {
+        StartSequence();
+    }
+}
