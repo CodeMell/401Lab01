5547c44d9de803144d8a600a24b1da58b080bd90
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-07-06 11:02:01 -0500

    Update Program.cs

--- 401Lab01/Program.cs
+++ 401Lab01/Program.cs
@@ -3,7 +3,8 @@ class labB
 {
     static void StartSequence()
     {
-        
+        Console.WriteLine("Enter a number greater than zero");
+        int gNum = Convert.ToInt32(Console.ReadLine());
     }
 
     static void Main(string[] args)
