0e342a038f45d503662a818a0abe3713f6d5412a
Renautis Patterson <123973708+CodeMell@users.noreply.github.com>
2023-07-06 15:29:39 -0500

    Update Program.cs

--- 401Lab01/Program.cs
+++ 401Lab01/Program.cs
@@ -8,9 +8,11 @@ class labB
         int[] arr = new int[arrayLength];
         for (int i = 0; i < arr.Length; i++)
         {
-            Console.WriteLine($"Please enter a number: {i} of {arrayLength}");
+            Console.WriteLine($"Please enter a number: {i+1} of {arrayLength}");
             arr[i] = Convert.ToInt32(Console.ReadLine());
         }
+        Console.WriteLine($"Please enter a random number beteen 1 and {arrayLength}");
+        int randomNum = Convert.ToInt32(Console.ReadLine());
     }
     static void Main(string[] args)
     {
